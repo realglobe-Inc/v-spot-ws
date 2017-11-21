e11db3e5410a99fa1b012a451d50098764971c00
okunishinishi <okunishinishi@gmail.com>
2017-11-21 14:43:13 +0900

    Implement server version

--- test/VSpotWSTest.js
+++ test/VSpotWSTest.js
@@ -5,8 +5,8 @@
 'use strict'
 
 const VSpotWS = require('../lib/VSpotWS')
-const { ok, equal } = require('assert')
-
+const {ok, equal} = require('assert')
+const aport = require('aport')
 describe('v-spot-w-s', () => {
   before(() => {
   })
@@ -14,8 +14,44 @@ describe('v-spot-w-s', () => {
   after(() => {
   })
 
-  it('Do test', () => {
+  it('Do test', async () => {
+    const port = await aport()
+    const server = new VSpotWS.Server()
+    const client01 = new VSpotWS.Client()
+    const client02 = new VSpotWS.Client()
+    try {
+      {
+        class Person {
+          async hi (msg) {
+            return `hi, ${msg}`
+          }
+        }
 
+        // Create a instance to a spot
+        const john = client01.load(Person, 'jp.realglobe.new-york.john')
+        await john.hi('I am in NewYork!')
+      }
+
+      await server.listen(port)
+
+      await client01.connect(`http://localhost:${port}`)
+      await client02.connect(`http://localhost:${port}`)
+
+      {
+        // Use remote instance
+        const john = await client02.use('jp.realglobe.new-york.john')
+        equal(
+          await john.hi('Calling from Japan!'),
+          'hi, Calling from Japan!'
+        )
+      }
+    } finally {
+
+      await client01.disconnect()
+      await client02.disconnect()
+
+      await server.close()
+    }
   })
 })
 
